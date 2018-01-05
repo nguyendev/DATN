d9fc72841d55040c46f0459b6c2776a0365b2738
nguyendev <nguyen.nah76@gmail.com>
2018-01-06 00:21:08 +0700

    form load

---
+++ Code/EyeForm/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EyeForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.image_PICBX = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.image_PICBX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image_PICBX
+            // 
+            this.image_PICBX.Location = new System.Drawing.Point(13, 29);
+            this.image_PICBX.Name = "image_PICBX";
+            this.image_PICBX.Size = new System.Drawing.Size(678, 400);
+            this.image_PICBX.TabIndex = 0;
+            this.image_PICBX.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 441);
+            this.Controls.Add(this.image_PICBX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.image_PICBX)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image_PICBX;
+    }
+}
+
--- Code/ThiToeic/Program.cs
+++ Code/ThiToeic/Program.cs
@@ -22,7 +22,7 @@ namespace ThiToeic
             BonusSkins.Register();
             SkinManager.EnableFormSkins();
             UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
-            Application.Run(new Form1());
+            Application.Run(new ToeicExam());
         }
     }
 }
